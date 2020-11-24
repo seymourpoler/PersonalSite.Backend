c6aff872867f9df400f7efbfe685ad490780f9e4
rodriguezl <rodriguezl@ryanair.com>
2020-11-24 14:08:22 +0100

    green return_ok

--- src/Pablo.PersonalSite.Web.Api/Controllers/HealthCheckController.cs
+++ src/Pablo.PersonalSite.Web.Api/Controllers/HealthCheckController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Pablo.PersonalSite.Web.Api.Controllers
 {
     public class HealthCheckController : Controller
     {
+        private readonly ILogger<HealthCheckController> logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            this.logger = logger;
+        }
+
         [HttpGet("/api/healthcheck")]
         public IActionResult GetStatus()
         {
-            return Ok();
+            logger.LogInformation("Status OK...");
+            return Ok("Pablo Company PersonalSite's API Rest");
         }
     }
 }
---
+++ test/Pablo.PersonalSite.Web.Api.Unit.Test/HealthCheckControllerShould.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using Pablo.PersonalSite.Web.Api.Controllers;
+using Shouldly;
+
+namespace Pablo.PersonalSite.Web.Api.Unit.Test
+{
+    [TestFixture]
+    public class HealthCheckControllerShould
+    {
+        [Test]
+        public void return_ok()
+        {
+            Mock<ILogger<HealthCheckController>> logger = new Mock<ILogger<HealthCheckController>>();
+            var controller = new HealthCheckController(logger.Object);
+
+            var response = controller.GetStatus() as OkObjectResult;
+
+            response.StatusCode.ShouldBe((int)HttpStatusCode.OK);
+        }
+    }
+}
