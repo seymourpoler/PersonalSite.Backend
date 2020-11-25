3d3ffa1a623fc88239f0ef79b715815bc8434692
rodriguezl <rodriguezl@ryanair.com>
2020-11-25 17:21:07 +0100

    adding FindJobExperienceRepository

---
+++ src/Pablo.PersonalSite.Persistence/JobExperience/Find/FindJobExperienceRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Pablo.PersonalSite.Domain.JobExperience.Find;
+
+namespace Pablo.PersonalSite.Persistence.JobExperience.Find
+{
+    public class FindJobExperienceRepository : IFindJobExpecienceRepository
+    {
+        public IReadOnlyList<Domain.JobExperience.Find.Models.JobExperience> Find()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
